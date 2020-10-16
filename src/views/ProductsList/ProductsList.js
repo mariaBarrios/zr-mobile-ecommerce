0f831c2b1cd045b3309aeefffb6fbb238cc7cdf8
maria_barrios <maria.barrios@biko2.com>
2020-10-16 02:46:23 +0200

    Adds InputSearch to ProductsListView

--- src/views/ProductsList/ProductsList.js
+++ src/views/ProductsList/ProductsList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from 'react-query'
 
 import { getProductsList } from '../../core/Products.service'
@@ -7,12 +7,19 @@ import { PageTitle } from '../_components/PageTitle/PageTitle'
 import { ProductItem } from './_components/ProductItem/ProductItem'
 import { ProductsListContainer } from './ProductList.styles'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import {
+  IconSearch,
+  InputSearchIconWrapper,
+  InputSearchInput,
+  InputSearchWrapper
+} from './_components/InputSearch/InputSearch.styles'
 
 export function ProductsList() {
   const { isLoading, isError, error, data: ProductList } = useQuery(
     'ProductsItems',
     getProductsList
   )
+  const [searchValue, setValue] = useState('')
 
   if (isLoading) {
     return <span>Cargando...</span>
@@ -26,10 +33,26 @@ export function ProductsList() {
     <>
       <section>
         <PageTitle>Consulta nuestro catálogo de móviles</PageTitle>
+        <InputSearchWrapper>
+          <InputSearchInput
+            type="text"
+            placeholder="Encuentra lo que buscas"
+            value={searchValue}
+            onChange={e => {
+              setValue(e.target.value)
+            }}
+            onFocus={e => (e.target.placeholder = '')}
+            onBlur={e =>
+              (e.target.placeholder = 'Encuentra lo que buscas')
+            }
+          />
+          <IconSearch />
+        </InputSearchWrapper>
       </section>
 
       <ProductsListContainer>
-        {ProductList?.map(productItem => (
+
+        {filterbySearchParameter(ProductList, searchValue).map(productItem => (
           <Link to={`/${productItem.id}`} key={productItem.id}>
             <ProductItem key={productItem.index} data={productItem} />
           </Link>
@@ -39,3 +62,14 @@ export function ProductsList() {
   )
 }
 
+function filterbySearchParameter(productList, searchValue) {
+  if (!searchValue) {
+    return productList
+  }
+
+  if (searchValue) {
+    return productList.filter(
+      item => item.brand.includes(searchValue) || item.model.includes(searchValue)
+    )
+  }
+}
---
+++ src/views/ProductsList/_components/InputSearch/InputSearch.styles.js
@@ -0,0 +1,53 @@
+import { rem } from 'polished'
+import styled from 'styled-components'
+import { media } from '../../../../ui/theme/media'
+import { colors, sizes } from '../../../../ui/theme/theme'
+
+import { ReactComponent as Search } from '../../../../ui/icon/search.svg'
+
+
+export const InputSearchWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+
+  padding: ${rem(sizes.mini)} ${rem(sizes.tiny)};
+  margin-bottom: ${rem(sizes.medium)};
+
+  border-bottom: 1px solid ${colors.lightGrey};
+
+  ${media.tablet`
+    width: ${rem(500)};
+    margin: 0 auto;
+    margin-bottom: ${rem(sizes.large)};
+
+ `}
+`
+
+export const InputSearchInput = styled.input`
+  width: 100%;
+
+  background: transparent;
+  border-color: transparent;
+  box-shadow: 0px;
+  outline: none;
+
+  letter-spacing: 2px;
+  text-transform: uppercase;
+
+  cursor: pointer;
+
+  &:focus {
+    cursor: auto;
+  }
+
+  &::placeholder {
+    color: ${colors.DarkGrey};
+    opacity: 0.6;
+  }
+`
+
+export const IconSearch = styled(Search)`
+  width: ${rem(sizes.base)};
+  height: ${rem(sizes.base)};
+`
