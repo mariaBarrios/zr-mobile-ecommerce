95299c94bc1c3e484d267f99c21a5d62bae542f4
maria_barrios <maria.barrios@biko2.com>
2020-10-16 02:49:28 +0200

    Adds fetch to API for ProductDetail

--- src/core/Products.service.js
+++ src/core/Products.service.js
@@ -6,3 +6,12 @@ export async function getProductsList() {
 
   return productList
 }
+
+
+export async function getProductDetail(id) {
+  const response = await fetch(`https://front-test-api.herokuapp.com/api/product/${id}`)
+
+  const productDetail = await response.json()
+
+  return productDetail
+}
