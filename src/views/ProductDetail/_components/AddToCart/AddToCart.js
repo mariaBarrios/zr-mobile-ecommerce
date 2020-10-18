c588f1062bec68bc357333d033bbd0917a208883
maria_barrios <maria.barrios@biko2.com>
2020-10-18 11:29:56 +0200

    Add Styles to AddToCart component

--- src/views/ProductDetail/_components/AddToCart/AddToCart.js
+++ src/views/ProductDetail/_components/AddToCart/AddToCart.js
@@ -1,38 +1,43 @@
 import React, { useState } from 'react'
+import { Button, Select } from './AddToCart.styles'
 
-export function AddToCart({storages, colors}) {
-  const [selectedColor, setSelectedColor] = useState(colors.lenght === 1 && colors.code[0])
-  const [selectedStorage, setSelectedStorage] = useState(colors.lenght === 1 && colors.code[0])
+export function AddToCart({ storages, colors }) {
+  const [selectedColor, setSelectedColor] = useState(
+    colors.length === 1 && colors[0].code
+  )
+  const [selectedStorage, setSelectedStorage] = useState(
+    storages.length === 1 && storages[0].code
+  )
 
   return (
     <>
-      <select
+      <Select
         value={selectedColor}
         onChange={e => setSelectedColor(e.target.value)}>
         {!selectedColor && <option>Elige un color</option>}
-
         {colors.map(color => (
           <option key={color.code} value={color.code}>
             {color.name}
           </option>
         ))}
-      </select>
+      </Select>
 
-      <select
+      <Select
         value={selectedStorage}
         onChange={e => setSelectedStorage(e.target.value)}>
-        {!selectedStorage && <option>Elije capacidad de almacenamiento</option>}
-
+        {!selectedStorage && <option>Elige capacidad de almacenamiento</option>}
         {storages.map(storage => (
           <option key={storage.code} value={storage.code}>
             {storage.name}
           </option>
         ))}
-      </select>
+      </Select>
 
-      <button disabled={!selectedColor || !selectedStorage}>
-        Añadir
-      </button>
+      <Button
+        onClick={() => console.log('Button click')}
+        disabled={!selectedColor || !selectedStorage}>
+        Añadir al carrito
+      </Button>
     </>
   )
 }
---
+++ src/views/ProductDetail/_components/AddToCart/AddToCart.styles.js
@@ -0,0 +1,52 @@
+import { rem } from 'polished'
+import styled, { css } from 'styled-components'
+import { colors, fontSizes, sizes } from '../../../../ui/theme/theme'
+
+export const Select = styled.select`
+  width: 80%;
+
+  padding-top: ${rem(sizes.small)};
+  padding-bottom: ${rem(sizes.small)};
+
+  background-color: white;
+  border: 0;
+  border-bottom: 1px solid ${colors.lightGrey};
+
+  font-family: inherit;
+  font-size: ${rem(fontSizes.base)};
+  letter-spacing: 2px;
+  color: ${colors.middleGrey};
+  text-transform: uppercase;
+`
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+
+  padding: ${rem(sizes.mini)} ${rem(sizes.base)};
+
+  border: 2px solid ${colors.middleTurquoise};
+  background-color: ${colors.middleTurquoise};
+
+  font-size: ${rem(fontSizes.base)};
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  color: ${colors.white};
+
+  opacity: ${p => p.disabled && 0.3};
+
+  ${p =>
+    !p.disabled &&
+    css`
+      cursor: pointer;
+
+      &:hover {
+        border: 2px solid ${colors.lightTurquoise};
+        background-color: ${colors.lightTurquoise};
+      }
+    `}
+`
