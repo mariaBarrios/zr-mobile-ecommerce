d8b168c1259b4c50af2661d63b555653aecc6722
maria_barrios <maria.barrios@biko2.com>
2020-10-18 11:32:43 +0200

    Add error management in Products.service

--- src/core/Products.service.js
+++ src/core/Products.service.js
@@ -2,14 +2,24 @@ export async function getProductsList() {
   const response = await fetch(
     'https://front-test-api.herokuapp.com/api/product'
   )
+
+  if (!response.ok) {
+    throw new Error('Network Error')
+  }
+
   const productList = await response.json()
 
   return productList
 }
 
-
 export async function getProductDetail(id) {
-  const response = await fetch(`https://front-test-api.herokuapp.com/api/product/${id}`)
+  const response = await fetch(
+    `https://front-test-api.herokuapp.com/api/product/${id}`
+  )
+
+  if (!response.ok) {
+    throw new Error('Network Error')
+  }
 
   const productDetail = await response.json()
 
